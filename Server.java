98a06150e860590b3d4807725801d6fe8b4ef24d
Maxim Klimenko <52748143+Infron@users.noreply.github.com>
2019-07-13 09:00:39 +0300

    Server Update
    Добавлены потоки для многопользовательской обработки. Сервер готов, НО нужны тесты для нескольких устройств.

--- Server.java
+++ Server.java
@@ -1,37 +1,86 @@
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.Serializable;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.io.Serializable;
+import java.net.SocketAddress;
+import java.rmi.ServerError;
+import java.sql.ClientInfoStatus;
+import java.util.ArrayList;
 
 public class Server {
     private static Socket ClientSocket;
-    private static ObjectInputStream objectInputStream;
-    private static ObjectOutputStream objectOutputStream;
+
+    public static ArrayList<userThread> userThreadList;
 
     public static void main(String[] args) throws IOException {
         System.out.println("Server starts!");
         ServerSocket Server = new ServerSocket(10000);
-        try {
-            while (true) {
-                ClientSocket = Server.accept();
-                try {
-                    objectInputStream = new ObjectInputStream(ClientSocket.getInputStream());
-                    Message newMessage = (Message) objectInputStream.readObject();
-                    if (newMessage.msg.equals("mamka")) break;
+        userThreadList = new ArrayList<>();
 
-                    objectOutputStream = new ObjectOutputStream(ClientSocket.getOutputStream());
-                    objectOutputStream.writeObject(newMessage);
-                    objectOutputStream.flush();
-                    System.out.println(newMessage.login + " --said-- " + newMessage.msg);
-                } catch (Exception e) {
-                    System.err.println(e);
+        try {
+            try {
+                while (true) {
+                    ClientSocket = Server.accept();
+                    System.out.println("User connected");
+                    userThreadList.add(new userThread((ClientSocket)));
                 }
+            } catch (IOException e){
+                ClientSocket.close();
             }
         } finally {
-            ClientSocket.close();
-            objectInputStream.close();
-            objectOutputStream.close();
+            Server.close();
         }
     }
+}
+
+class Message implements Serializable {
+    String login;
+    String msg;
+
+    Message(String log, String mes) {
+        this.login = log;
+        this.msg = mes;
+    }
+}
+
+class userThread extends Thread{
+    private Socket ClientSocket;
+    private ObjectInputStream objectInputStream;
+    private ObjectOutputStream objectOutputStream;
+
+    userThread(Socket socket) throws IOException {
+        this.ClientSocket = socket;
+
+        start();
+    }
+
+    public void run(){
+        try{
+            while (true){
+                objectInputStream = new ObjectInputStream(ClientSocket.getInputStream());
+                objectOutputStream = new ObjectOutputStream(ClientSocket.getOutputStream());
+                Message newMessage = (Message) objectInputStream.readObject();
+                if (newMessage.msg.equals("mamka")) break;
+
+                    for (int i = 0; i < Server.userThreadList.size(); i++) {
+                        Server.userThreadList.get(i).sendMessage(newMessage);
+                    }
+            }
+        } catch (Exception e) {
+            System.err.println(e);
+        }
+    }
+
+    private void sendMessage(Message msg) {
+        try{
+                objectOutputStream.writeObject(msg);
+                objectOutputStream.flush();
+        } catch (IOException e){
+            System.err.println(e);
+        }
+    }
+
 }
